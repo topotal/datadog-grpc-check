873d749fa6311c6f78d353681dfadd9a55cfc3c7
Takatada Yoshima <yoshima@topotal.com>
2022-04-06 10:21:12 +0900

    Add tests for tags

--- tests/test_grpc_health_probe.py
+++ tests/test_grpc_health_probe.py
@@ -41,6 +41,21 @@ class TestGrpcHealthProbe(unittest.TestCase):
         m_gauge.assert_any_call('network.grpc.response_time', ANY, tags=expected_tags)
 
     @patch('grpc_check.GrpcCheck._gauge')
+    def test_grpc_health_probe_healty_with_tags(self, m_gauge):
+        instance = {
+            'server': 'localhost',
+            'port': 50051,
+            'service': 'helloworld.GreeterHealthy',
+            'tags': ['key1:val1', 'key2:val2'],
+        }
+        check = grpc_check.GrpcCheck('grpc_check', {}, [instance])
+        check.check(instance)
+
+        expected_tags = ['key1:val1', 'key2:val2', 'addr:localhost:50051', 'service:helloworld.GreeterHealthy']
+        m_gauge.assert_any_call('network.grpc.can_connect', 1, tags=expected_tags)
+        m_gauge.assert_any_call('network.grpc.response_time', ANY, tags=expected_tags)
+
+    @patch('grpc_check.GrpcCheck._gauge')
     def test_grpc_health_probe_unhealty(self, m_gauge):
         instance = {
             'server': 'localhost',
@@ -51,6 +66,20 @@ class TestGrpcHealthProbe(unittest.TestCase):
         check.check(instance)
 
         expected_tags = ['addr:localhost:50051', 'service:helloworld.GreeterUnhealthy']
+        m_gauge.assert_any_call('network.grpc.can_connect', 0, tags=expected_tags)
+
+    @patch('grpc_check.GrpcCheck._gauge')
+    def test_grpc_health_probe_unhealty_with_tags(self, m_gauge):
+        instance = {
+            'server': 'localhost',
+            'port': 50051,
+            'service': 'helloworld.GreeterUnhealthy',
+            'tags': ['key1:val1', 'key2:val2'],
+        }
+        check = grpc_check.GrpcCheck('grpc_check', {}, [instance])
+        check.check(instance)
+
+        expected_tags = ['key1:val1', 'key2:val2', 'addr:localhost:50051', 'service:helloworld.GreeterUnhealthy']
         m_gauge.assert_any_call('network.grpc.can_connect', 0, tags=expected_tags)
 
     def test_grpc_health_probe_invalid_option(self):
